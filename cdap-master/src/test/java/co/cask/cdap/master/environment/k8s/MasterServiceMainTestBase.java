b6b28d706ad60f24b88c6dbffeedd2641939b941
Terence Yim <terencey@google.com>
2019-02-19 13:50:13 -0800

    (CDAP-14646) Added MetadataServiceMain for the K8s

--- cdap-master/src/test/java/co/cask/cdap/master/environment/k8s/MasterServiceMainTestBase.java
+++ cdap-master/src/test/java/co/cask/cdap/master/environment/k8s/MasterServiceMainTestBase.java
@@ -17,27 +17,39 @@
 package co.cask.cdap.master.environment.k8s;
 
 import co.cask.cdap.api.dataset.DatasetDefinition;
+import co.cask.cdap.app.guice.ConstantTransactionSystemClient;
 import co.cask.cdap.common.conf.CConfiguration;
 import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.guice.ConfigModule;
+import co.cask.cdap.common.guice.InMemoryDiscoveryModule;
+import co.cask.cdap.data.runtime.DataSetsModules;
+import co.cask.cdap.data.runtime.StorageModule;
 import co.cask.cdap.data.runtime.SystemDatasetRuntimeModule;
 import co.cask.cdap.data2.dataset2.lib.table.leveldb.LevelDBTableService;
+import co.cask.cdap.data2.metadata.store.MetadataStore;
 import co.cask.cdap.data2.nosql.NoSqlStructuredTableAdmin;
 import co.cask.cdap.data2.nosql.NoSqlStructuredTableRegistry;
+import co.cask.cdap.security.auth.context.AuthenticationContextModules;
+import co.cask.cdap.security.spi.authorization.AuthorizationEnforcer;
+import co.cask.cdap.security.spi.authorization.NoOpAuthorizer;
 import co.cask.cdap.spi.data.StructuredTableAdmin;
 import co.cask.cdap.spi.data.table.StructuredTableRegistry;
 import co.cask.cdap.store.StoreDefinition;
 import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
+import org.apache.tephra.TransactionSystemClient;
 import org.apache.twill.common.Cancellable;
 import org.apache.twill.internal.zookeeper.InMemoryZKServer;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.Writer;
@@ -55,6 +67,8 @@ public class MasterServiceMainTestBase {
 
   @ClassRule
   public static final TemporaryFolder TEMP_FOLDER = new TemporaryFolder();
+
+  private static final Logger LOG = LoggerFactory.getLogger(MasterServiceMainTestBase.class);
 
   private static InMemoryZKServer zkServer;
   private static Map<Class<?>, ServiceMainManager<?>> serviceManagers = new HashMap<>();
@@ -81,6 +95,7 @@ public class MasterServiceMainTestBase {
     serviceManagers.put(MessagingServiceMain.class, runMain(MessagingServiceMain.class, cConf));
     serviceManagers.put(MetricsServiceMain.class, runMain(MetricsServiceMain.class, cConf));
     serviceManagers.put(AppFabricServiceMain.class, runMain(AppFabricServiceMain.class, cConf));
+    serviceManagers.put(MetadataServiceMain.class, runMain(MetadataServiceMain.class, cConf));
   }
 
   @AfterClass
@@ -120,16 +135,33 @@ public class MasterServiceMainTestBase {
     CConfiguration cConf = CConfiguration.copy(originalCConf);
     cConf.set(Constants.CFG_LOCAL_DATA_DIR, TEMP_FOLDER.newFolder().getAbsolutePath());
 
-    // Create StructuredTable stores before starting the main. The registry will be preserved and pick by the main class
+    // Create StructuredTable stores before starting the main.
+    // The registry will be preserved and pick by the main class.
+    // Also try to create metadata tables.
     Injector injector = Guice.createInjector(
       new ConfigModule(cConf),
-      new SystemDatasetRuntimeModule().getStandaloneModules()
+      new SystemDatasetRuntimeModule().getStandaloneModules(),
+      // We actually only need the MetadataStore createIndex.
+      // But due to the DataSetsModules, we need to pull in more modules.
+      new DataSetsModules().getStandaloneModules(),
+      new InMemoryDiscoveryModule(),
+      new StorageModule(),
+      new AuthenticationContextModules().getNoOpModule(),
+      new AbstractModule() {
+        @Override
+        protected void configure() {
+          bind(AuthorizationEnforcer.class).to(NoOpAuthorizer.class);
+          bind(TransactionSystemClient.class).to(ConstantTransactionSystemClient.class);
+        }
+      }
     );
     DatasetDefinition tableDef = injector.getInstance(Key.get(DatasetDefinition.class,
                                                               Names.named(Constants.Dataset.TABLE_TYPE_NO_TX)));
     StructuredTableRegistry tableRegistry = new NoSqlStructuredTableRegistry(tableDef);
     StructuredTableAdmin tableAdmin = new NoSqlStructuredTableAdmin(tableDef, tableRegistry);
     StoreDefinition.createAllTables(tableAdmin, tableRegistry);
+
+    injector.getInstance(MetadataStore.class).createIndex();
     injector.getInstance(LevelDBTableService.class).close();
 
     // Write the "cdap-site.xml" and pass the directory to the main service
